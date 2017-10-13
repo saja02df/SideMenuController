a6ca97182fef145f0d5f3c41213b6b7eb887da81
Saurabh Jain <s.saurabh.j.jain@oracle.com>
2017-10-13 14:00:09 +0530

    - Test

--- Source/SideContainmentSegue.swift
+++ Source/SideContainmentSegue.swift
@@ -22,7 +22,7 @@
 //  SOFTWARE.
 
 open class SideContainmentSegue: UIStoryboardSegue{
-    //SJ:: v1.2.5
+    //SJ:: v1.2.6
     override open func perform() {
         if let sideController = self.source as? SideMenuController {
             sideController.embed(sideViewController: destination)
