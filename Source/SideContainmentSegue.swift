f0858987ece3a1d271e78b5e922174e718151cb3
Saurabh Jain <s.saurabh.j.jain@oracle.com>
2017-10-13 12:28:15 +0530

    - Test push again...

--- Source/SideContainmentSegue.swift
+++ Source/SideContainmentSegue.swift
@@ -22,7 +22,7 @@
 //  SOFTWARE.
 
 open class SideContainmentSegue: UIStoryboardSegue{
-    //SJ:
+    //SJ::
     override open func perform() {
         if let sideController = self.source as? SideMenuController {
             sideController.embed(sideViewController: destination)
