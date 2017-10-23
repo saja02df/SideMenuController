09d85dde47d16f792ce0dfc6c060eeadaf912faf
Saurabh Jain <s.saurabh.j.jain@oracle.com>
2017-10-23 12:55:21 +0530

    - Test Proxy

--- Source/SideContainmentSegue.swift
+++ Source/SideContainmentSegue.swift
@@ -22,7 +22,7 @@
 //  SOFTWARE.
 
 open class SideContainmentSegue: UIStoryboardSegue{
-    //SJ:: v1.2.8
+    //SJ:: v1.2.9
     override open func perform() {
         if let sideController = self.source as? SideMenuController {
             sideController.embed(sideViewController: destination)
